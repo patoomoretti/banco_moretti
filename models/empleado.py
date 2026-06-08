db91c27c9b7022542545188940d5b853e3b3f744
patoomoretti <patriciomoretti@hotmail.com>
2026-06-08 11:42:03 -0300

    Limpieza de estructura antigua

--- models/empleado.py
+++
@@ -1,9 +0,0 @@
-class Empleado:
-    def __init__(self,nombre,apellido,dni,sector):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.dni = dni
-        self.sector = sector
-        
-    def __str__(self):
-        return f"{self.nombre} {self.apellido}"
--- models/usuario.py
+++
@@ -1,8 +0,0 @@
-class Usuario:
-    def __init__(self, dni, pin):
-        self.dni = dni
-        self.pin = pin
-        
-    def __str__(self):
-        return f"Usuario: {self.dni})"
-    
